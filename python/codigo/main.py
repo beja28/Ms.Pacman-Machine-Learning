9ddaa4f6d4366675b217bfbc90dc88038effe0ef
DaniiJim <danjim04@ucm.es>
2024-10-24 20:29:48 +0200

    Uso de rutas Relativas
    Pasamos de usar rutas Absolutas a rutas Relativas para acceder a las carpetas internas del proyecto

--- python/codigo/main.py
+++ python/codigo/main.py
@@ -1,15 +1,39 @@
 from preprocessing import preprocess_csv
 from models import train_pytorch_nn, save_model_pth, cross_validate_sklearn_mlp, save_model_mlp
 import torch
+import os
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
 
-# Ruta de datos y de la carpeta para guardar modelos
-path = 'D:/Documentos/diego/universidad/4 Curso/TFG/Ms.Pacman-Machine-Learning/DataSets/01_gameStatesData.csv'
-path_trained = 'D:/Documentos/diego/universidad/4 Curso/TFG/Ms.Pacman-Machine-Learning/python/codigo/Redes_Entrenadas'
+# Ruta de datos y de la carpeta para guardar modelos --> RUTA ABSOLUTA --> PROBLEMA
+# path = 'D:/Documentos/diego/universidad/4 Curso/TFG/Ms.Pacman-Machine-Learning/DataSets/01_gameStatesData.csv'
+# path_trained = 'D:/Documentos/diego/universidad/4 Curso/TFG/Ms.Pacman-Machine-Learning/python/codigo/Redes_Entrenadas'
+
+
+
+# --- CREAR PATHS USANDO RUTAS RELATIVAS ---
+
+# Obtener la ruta de la carpeta 'codigo' donde está 'main.py'
+directorio_actual = os.path.dirname(os.path.abspath(__file__))
+
+# Construir la ruta que sube dos niveles desde 'codigo' y entra en 'DataSets'
+dataset_path = os.path.join(directorio_actual, '..', '..', 'DataSets', '01_gameStatesData.csv')
+
+# Normalizar la ruta para evitar problemas con distintos sistemas operativos
+dataset_path = os.path.normpath(dataset_path)
+
+# Construir la ruta hacia la carpeta 'Redes_Entrenadas'
+path_trained = os.path.join(directorio_actual, 'Redes_Entrenadas')
+
+# Normalizar la ruta para evitar problemas con distintos sistemas operativos
+path_trained = os.path.normpath(path_trained)
+
+
+
+# --- PREPROCESAMIENTO ---
 
 # Preprocesamiento del CSV
-X, Y = preprocess_csv(path)
+X, Y = preprocess_csv(dataset_path)
 
 # Configuramos los parámetros de la red
 n_features = X.shape[1]
@@ -29,6 +53,10 @@ Y_cv_tensor = torch.tensor(y_cv.values, dtype=torch.float32)
 train_dataset = TensorDataset(X_train_tensor, Y_train_tensor)
 train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
 
+
+
+# --- ENTRENAMIENTO DE REDES NEURONALES ---
+
 # Entrenar el modelo con PyTorch
 pytorch_model = train_pytorch_nn(X_train_tensor, Y_train_tensor, X_cv_tensor, Y_cv_tensor, n_features, n_classes, train_loader)
 save_model_pth(pytorch_model, path_trained)
@@ -36,3 +64,4 @@ save_model_pth(pytorch_model, path_trained)
 # Entrenar con MLP de Scikit-learn
 mlp_model = cross_validate_sklearn_mlp(X, Y)
 save_model_mlp(mlp_model, path_trained)
+
