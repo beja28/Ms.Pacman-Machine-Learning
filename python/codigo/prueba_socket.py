ff5ca42b6017155c97bea10fb8db83ecc21d4d7f
dieggoglz <114392493+dieggoglz@users.noreply.github.com>
2024-10-30 23:53:31 +0100

    Division modelos, procesar estado de juego y creacion socket
    Se ha creado un socket que recibe desde java un estado del juego, se envia a una clase que lo preprocesa y se devuelve a java la prediccion del movimiento, sacada por el modelo entrenado

--- python/codigo/prueba_socket.py
+++ python/codigo/prueba_socket.py
@@ -1,5 +1,52 @@
+import os
 import socket
-import json
+import joblib
+import torch
+from preprocessing import preprocess_game_state
+from model_pytorch import MyModelPyTorch
+from main import n_features, n_classes 
+from config import Config
+
+
+# --- CREAR PATHS USANDO RUTAS RELATIVAS ---
+
+# Obtener la ruta de la carpeta 'codigo' donde está 'main.py'
+directorio_actual = os.path.dirname(os.path.abspath(__file__))
+
+# Construir la ruta que sube dos niveles desde 'codigo' y entra en 'DataSets'
+dataset_path = os.path.join(directorio_actual, '..', '..', 'DataSets', '01_gameStatesData.csv')
+
+# Normalizar la ruta para evitar problemas con distintos sistemas operativos
+dataset_path = os.path.normpath(dataset_path)
+
+# Construir la ruta hacia la carpeta 'Redes_Entrenadas'
+path_trained = os.path.join(directorio_actual, 'Redes_Entrenadas')
+
+# Normalizar la ruta para evitar problemas con distintos sistemas operativos
+path_trained = os.path.normpath(path_trained)
+
+
+
+# """
+#     Cargar modelo sklearn
+# """
+# # Cargar el modelo
+# model_filename = 'mlp_trained_model_2024-10-24.pkl'  # Cambia 'mi_modelo' por el nombre que desees
+# full_model_path = os.path.join(path_trained, model_filename)
+
+# # Cargar el modelo entrenado
+# mlp_model = joblib.load(full_model_path)
+
+
+"""
+    Cargar modelo PyTorch
+"""
+model_filename = 'pytorch_model_2024-10-30.pth'
+full_model_path = os.path.join(path_trained, model_filename)
+modelPytorch = MyModelPyTorch(n_features, n_classes)
+
+modelPytorch.load_state_dict(torch.load(full_model_path, weights_only=True))
+modelPytorch.eval()
 
 #Configuración
 HOST = 'localhost'  # Podemos poner una IP o localhost
@@ -26,10 +73,23 @@ while True:
 
     mensaje = data.decode('utf-8')
     print(f"Datos recibidos: {mensaje}")
+    
+    preprocessed_state = preprocess_game_state(mensaje, dataset_path)
+    
+    #print(preprocessed_state)
+    
+    # Convertir el estado preprocesado a tensor
+    input_tensor = torch.tensor(preprocessed_state, dtype=torch.float32).unsqueeze(0)  # Agregar dimensión para batch (modelo espera recibir un batch de un solo ejemplo)
+    
+    print(n_features)
+    
+    # Realizar la predicción
+    with torch.no_grad():  # No calcular gradientes
+        prediccion = modelPytorch(input_tensor)
 
     # Responder con algo diferente según lo recibido
     if mensaje:
-        respuesta = f"NEUTRAL\n"  # Agrega un salto de línea al final
+        respuesta = f"{prediccion}\n"  # Agrega un salto de línea al final
         conn.sendall(respuesta.encode('utf-8'))  # Enviar respuesta codificada en UTF-8
 
 conn.close()
--- python/setIntersecciones.py
+++ python/setIntersecciones.py
@@ -1,24 +1,7 @@
 import pandas as pd
-import os
 
-
-"""
-EL DATASET DEBE CONTENER 38 POSICIONES DE INTERSECCIONES DISTINTAS
-"""
-
-# Obtener la ruta de la carpeta 'codigo' donde está 'main.py'
-directorio_actual = os.path.dirname(os.path.abspath(__file__))
-
-# Construir la ruta que sube dos niveles desde 'codigo' y entra en 'DataSets'
-dataset_path = os.path.join(directorio_actual, '..', 'DataSets', '03_gameStatesData.csv')
-
-# Normalizar la ruta para evitar problemas con distintos sistemas operativos
-dataset_path = os.path.normpath(dataset_path)
-
-df = pd.read_csv(dataset_path)
-
+df = pd.read_csv('01_gameStatesData.csv')
 
 valores_unicos = sorted(set(df['pacmanCurrentNodeIndex'].unique()))
 
- 
-print(len(valores_unicos))
+print(valores_unicos)
