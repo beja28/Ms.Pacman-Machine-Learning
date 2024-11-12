6e9f68e15f6fb720c0243ed3355c66ea3a46f9e0
dieggoglz <114392493+dieggoglz@users.noreply.github.com>
2024-11-12 20:32:42 +0100

    Comandos en main y eliminacion de Config
    El codigo ahora se ejecuta todo desde el main por medio de comandos. Elimine la clase Config porque de momento no la veo necesaria  para implementar las rutas relativas

--- python/codigo/main.py
+++ python/codigo/main.py
@@ -1,16 +1,12 @@
 from preprocessing import preprocess_csv
-#from model_pytorch import train_pytorch_nn, save_model_pth
-#from model_sklearn import MLPModel
+from model_pytorch import train_pytorch_nn, save_model_pth
+from model_sklearn import MLPModel
+from prueba_socket import start_socket 
 import torch
 import os
 from sklearn.model_selection import train_test_split
 from torch.utils.data import TensorDataset, DataLoader
-
-# Ruta de datos y de la carpeta para guardar modelos --> RUTA ABSOLUTA --> PROBLEMA
-# path = 'D:/Documentos/diego/universidad/4 Curso/TFG/Ms.Pacman-Machine-Learning/DataSets/01_gameStatesData.csv'
-# path_trained = 'D:/Documentos/diego/universidad/4 Curso/TFG/Ms.Pacman-Machine-Learning/python/codigo/Redes_Entrenadas'
-
-
+import argparse
 
 # --- CREAR PATHS USANDO RUTAS RELATIVAS ---
 
@@ -56,14 +52,39 @@ train_dataset = TensorDataset(X_train_tensor, Y_train_tensor)
 train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
 
 
+def main():
+    parser = argparse.ArgumentParser(description="Selecciona si quieres entrenar un modelo o utilizar uno para enviar la predicción")
+    
+    # Definición de comandos
+    subparsers = parser.add_subparsers(dest="command", help="Comandos disponibles") # Lo que se seleccione se guarda en command
 
-# --- ENTRENAMIENTO DE REDES NEURONALES ---
+    # Comando para seleccionar el modelo para las predicciones
+    parser_select_model = subparsers.add_parser("model", help="Selecciona el modelo a elegir para sacar la predicción (pytorch | sklearn)")
+    parser_select_model.add_argument("model", choices=["pytorch", "sklearn"], help="Para usar el modelo Pytorch escriba -> model pytorch | Para usar el modelo MLP de scikit-learn escriba -> model sklearn")
 
-# # Entrenar el modelo con PyTorch
-#pytorch_model = train_pytorch_nn(X_cv_tensor, Y_cv_tensor, train_loader, n_features, n_classes)
-#save_model_pth(pytorch_model, path_trained)
+    # Comando para entrenar el modelo
+    parser_train_model = subparsers.add_parser("train_model", help="Elige el modelo a entrenar")
+    parser_train_model.add_argument("train_model", choices=["pytorch", "sklearn"], help="Para entrenar el modelo Pytorch escriba -> train_model pytorch | Para entrenar el modelo MLP de scikit-learn escriba -> train_model sklearn")
+    
+    args = parser.parse_args()
 
-# # Entrenar con MLP de Scikit-learn
-# mlp_model = MLPModel()
-# mlp_model.train_and_cross_validate(X, Y)
-# mlp_model.save_model(path_trained)
+    
+    if args.command == "model":
+        start_socket(args.model, n_features, n_classes)
+        
+    elif args.command == "train_model":
+        if args.train_model == "pytorch":
+            """ Entrenar el modelo con PyTorch """
+            pytorch_model = train_pytorch_nn(X_cv_tensor, Y_cv_tensor, train_loader, n_features, n_classes)
+            save_model_pth(pytorch_model, path_trained)
+            
+        elif args.train_model == "sklearn":
+            """ Entrenar con MLP de Scikit-learn """
+            mlp_model = MLPModel()
+            mlp_model.train_and_cross_validate(X, Y)
+            mlp_model.save_model(path_trained)
+
+
+if __name__ == "__main__":
+    main()
+    
