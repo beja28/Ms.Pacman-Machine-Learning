f70e1962e22b71561da469822235d2b4520d84f5
dieggoglz <114392493+dieggoglz@users.noreply.github.com>
2024-11-12 20:32:42 +0100

    Comandos en main y eliminacion de Config
    El codigo ahora se ejecuta todo desde el main por medio de comandos. Elimine la clase Config porque de momento no la veo necesaria  para implementar las rutas relativas

--- python/codigo/prueba_socket.py
+++ python/codigo/prueba_socket.py
@@ -4,17 +4,9 @@ import joblib
 import torch
 from preprocessing import preprocess_game_state
 from model_pytorch import MyModelPyTorch
-from main import n_features, n_classes 
-from config import Config
-import argparse
 import numpy as np
 
-# Utilización de argparse para seleccionar el modelo que queramos por linea de comandos
-parser = argparse.ArgumentParser(description="Selecciona el modelo que desees utilizar para sacar la predicción del movimiento")
-parser.add_argument('--model', choices=['pytorch', 'sklearn'], required=True, help="Para usar el modelo Pytorch escriba -> pytorch | Para usar el modelo MLP de scikit-learn escriba -> sklearn")
-arg = parser.parse_args()
-
-# --- CREAR PATHS USANDO RUTAS RELATIVAS ---
+""" --- CREAR PATHS USANDO RUTAS RELATIVAS --- """
 
 # Obtener la ruta de la carpeta 'codigo' donde está 'main.py'
 directorio_actual = os.path.dirname(os.path.abspath(__file__))
@@ -32,61 +24,42 @@ path_trained = os.path.join(directorio_actual, 'Redes_Entrenadas')
 path_trained = os.path.normpath(path_trained)
 
 
-"""
-Cargar el modelo deseado
-"""
-if arg.model == 'sklearn':
-    model_filename= 'mlp_trained_model_2024-11-07.pkl' 
-    full_model_path = os.path.join(path_trained, model_filename)
-    mlp_model = joblib.load(full_model_path)  
-elif arg.model == 'pytorch':
-    model_filename = 'pytorch_model_2024-10-31.pth'
-    full_model_path = os.path.join(path_trained, model_filename)
-    modelPytorch = MyModelPyTorch(n_features, n_classes)
-    modelPytorch.load_state_dict(torch.load(full_model_path, weights_only=True))
-    modelPytorch.eval()
+
+def model_for_prediction(model_type,n_features, n_classes):
+
+    """
+    Cargar el modelo deseado
+    """
+    if model_type == 'sklearn':
+        model_filename= 'mlp_trained_model_2024-11-07.pkl' 
+        full_model_path = os.path.join(path_trained, model_filename)
+        mlp_model = joblib.load(full_model_path)  
+        return mlp_model, None
+    elif model_type == 'pytorch':
+        model_filename = 'pytorch_model_2024-10-31.pth'
+        full_model_path = os.path.join(path_trained, model_filename)
+        modelPytorch = MyModelPyTorch(n_features, n_classes)
+        modelPytorch.load_state_dict(torch.load(full_model_path, weights_only=True))
+        modelPytorch.eval()
+        return None, modelPytorch
+    else:
+            raise ValueError("Modelo no reconocido. Elige 'pytorch' o 'sklearn'.")
 
 
-
-
-#Configuración
-HOST = 'localhost'  # Podemos poner una IP o localhost
-PORT = 12345        # Puerto que queramos usar
-
-#Crear socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-
-#Escuchar conexiones entrantes
-server_socket.listen()
-
-print(f"Servidor escuchando en {HOST}:{PORT}")
-
-#Aceptar conexión
-conn, addr = server_socket.accept()
-print(f"Conectado con {addr}")
-
-#Recibir datos
-while True:
-    data = conn.recv(1024)  # El 1024 es el número máximo de bytes que se intenta recibir
-    if not data:
-        break
-
-    mensaje = data.decode('utf-8')
-    print(f"Datos recibidos: {mensaje}")
+def get_prediction(model_type, mensaje, mlp_model=None, modelPytorch=None):
     
     preprocessed_state = preprocess_game_state(mensaje, dataset_path)
     
     # Convertir el estado preprocesado a tensor o array, segun el modelo
     
-    if arg.model == 'pytorch':
+    if model_type == 'pytorch':
         input_tensor = torch.tensor(preprocessed_state.values, dtype=torch.float32).unsqueeze(0)  # Agregar dimensión para batch (modelo espera recibir un batch de un solo ejemplo)
         # Realizar la predicción
         with torch.no_grad():  # No calcular gradientes
             prediccion = modelPytorch(input_tensor)               
         # Seleccionar el índice con el valor máximo para obtener el movimiento predicho
         predicted_index = torch.argmax(prediccion)      
-    elif arg.model == 'sklearn':
+    elif model_type == 'sklearn':
         prediccion = mlp_model.predict(preprocessed_state) 
         predicted_index = np.argmax(prediccion) # Como prediccion no es un tensor si no una lista en este caso, usamos np en vez de torch
     
@@ -94,7 +67,43 @@ while True:
     moves = ['RIGHT', 'LEFT', 'UP', 'DOWN', 'NEUTRAL']
     predicted_move = moves[predicted_index]
     
-    respuesta = f"{predicted_move}\n" 
-    conn.sendall(respuesta.encode('utf-8'))  # Enviar respuesta codificada en UTF-8
+    return predicted_move
+    
+    
+def start_socket(model_type, n_features, n_classes):
+    #Configuración
+    HOST = 'localhost'  # Podemos poner una IP o localhost
+    PORT = 12345        # Puerto que queramos usar
 
-conn.close()
+    #Crear socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+
+    #Escuchar conexiones entrantes
+    server_socket.listen()
+
+    print(f"Servidor escuchando en {HOST}:{PORT}")
+
+    #Aceptar conexión
+    conn, addr = server_socket.accept()
+    print(f"Conectado con {addr}")
+
+    # Cargar el modelo para la predicción
+    mlp_model, modelPytorch = model_for_prediction(model_type, n_features, n_classes)
+
+    #Recibir datos
+    while True:
+        data = conn.recv(1024)  # El 1024 es el número máximo de bytes que se intenta recibir
+        if not data:
+            break
+
+        mensaje = data.decode('utf-8')
+        print(f"Datos recibidos: {mensaje}")
+        
+        # Obtener la predicción usando el modelo seleccionado
+        predicted_move = get_prediction(model_type, mensaje, mlp_model, modelPytorch)
+        
+        respuesta = f"{predicted_move}\n" 
+        conn.sendall(respuesta.encode('utf-8'))  # Enviar respuesta codificada en UTF-8
+
+    conn.close()
