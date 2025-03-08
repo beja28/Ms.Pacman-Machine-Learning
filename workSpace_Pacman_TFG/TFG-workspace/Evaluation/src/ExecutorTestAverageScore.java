e7e35be7e80e4f2a2f34cf9ee423837668ad1b57
DaniiJim <danjim04@ucm.es>
2025-03-08 15:44:36 +0100

    Socket mejorado y función para calcular score medio del modelo

---
+++ workSpace_Pacman_TFG/TFG-workspace/Evaluation/src/ExecutorTestAverageScore.java
@@ -0,0 +1,59 @@
+import pacman.ExecutorModes;
+import pacman.controllers.GhostController;
+import pacman.controllers.HumanController;
+import pacman.controllers.KeyBoardInput;
+import pacman.controllers.PacmanController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.ucm.fdi.ici.TFGpacman.PacManNeuro;
+
+
+/*	ICI HALL-OF-FAME
+
+--- Best MsPacMan (score) --- 
+es.ucm.fdi.ici.c2223.practica1.grupo06.MsPacMan (9958)
+es.ucm.fdi.ici.c2122.practica1.grupo10.MsPacMan (7164)
+es.ucm.fdi.ici.c2324.practica1.grupo07.MsPacMan (5378)
+
+
+es.ucm.fdi.ici.c2223.practica2.grupo02.MsPacMan (7000) 
+es.ucm.fdi.ici.c2122.practica2.grupo01.MsPacMan (6517) 
+es.ucm.fdi.ici.c2324.practica2.grupo01.MsPacMan (4452) 
+
+
+--- Best Ghosts (score) --- 
+es.ucm.fdi.ici.c2223.practica1.grupo06.Ghosts (2064)
+es.ucm.fdi.ici.c2122.practica1.grupo01.Ghosts (2108)
+es.ucm.fdi.ici.c2324.practica1.grupo08.Ghosts (2152)
+
+es.ucm.fdi.ici.c2324.practica2.grupo04.Ghosts (1924)
+es.ucm.fdi.ici.c2122.practica2.grupo01.Ghosts (2648)
+es.ucm.fdi.ici.c2223.practica2.grupo02.Ghosts (3704)
+
+
+ */
+
+
+
+public class ExecutorTestAverageScore {
+
+    public static void main(String[] args) {
+    	ExecutorModes executor = new ExecutorModes.Builder()
+                .setTickLimit(4000)
+                .setVisual(true)
+                .setScaleFactor(2.5)
+                .build();
+
+        PacmanController pacMan = new PacManNeuro();
+        List<GhostController> ghostControllers = new ArrayList<>();
+        ghostControllers.add(new es.ucm.fdi.ici.c2324.practica1.grupo06.Ghosts());
+        ghostControllers.add(new es.ucm.fdi.ici.c2324.practica1.grupo01.Ghosts());
+        ghostControllers.add(new es.ucm.fdi.ici.c2324.practica2.grupo04.Ghosts());
+        ghostControllers.add(new es.ucm.fdi.ici.c2324.practica2.grupo01.Ghosts());
+        
+        executor.runGameCalculateAverageScore(pacMan, ghostControllers, 10, 1000); //last parameter defines speed     
+    }
+	
+}
