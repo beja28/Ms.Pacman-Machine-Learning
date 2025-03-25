dde871cf3561a510434ce2de8ae77eee9de47ce2
DaniiJim <danjim04@ucm.es>
2025-03-25 10:50:50 +0100

    Modelos obsoletos eliminados + archivo para apuntar info modelos
    He borrado modelos que estaban obsoletos, y además se crea el archivo .md dentro de la carpeta de modelos entrenados para anotar información sobre cada red (fecha, tipo, dataset, etc.).
    De esta forma, evitamos depender de la memoria y mantenemos un registro claro de la configuración y usos de cada modelo.

--- python/codigo/main.py
+++ python/codigo/main.py
@@ -123,7 +123,7 @@ def main():
         Importante seleccionar la carpeta de los modelos sklearn o pytorch para que funcione correctamente
         --------------------------------------------------------------------------------------------------
         """
-        model_directory = os.path.join(path_trained, "models_2025-03-12")
+        model_directory = os.path.join(path_trained, "models_2025-03-05")
         
         # Obtener todos los archivos con extensión .pkl o .pth
         model_files = [f for f in os.listdir(model_directory) if f.endswith(('.pkl', '.pth'))]
--- python/codigo/prueba_socket.py
+++ python/codigo/prueba_socket.py
@@ -16,7 +16,7 @@ import numpy as np
 directorio_actual = os.path.dirname(os.path.abspath(__file__))
 
 # Construir la ruta que sube dos niveles desde 'codigo' y entra en 'DataSets'
-dataset_path = os.path.join(directorio_actual, '..', '..', 'DataSets', '07_gameStatesData.csv')
+dataset_path = os.path.join(directorio_actual, '..', '..', 'DataSets', '06_gameStatesData.csv')
 
 # Normalizar la ruta para evitar problemas con distintos sistemas operativos
 dataset_path = os.path.normpath(dataset_path)
